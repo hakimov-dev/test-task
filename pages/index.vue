ad55ad73d8b37238cc7518ae8ab8bc526c55cdac
Hakimov-dev <webhacker0100@gmail.com>
2024-11-23 20:17:14 +0500

    Created new function to get data and cleared variable from dummy data

--- pages/index.vue
+++ pages/index.vue
@@ -14,45 +14,36 @@
         >
       </transition>
     </div>
-    <div class="mt-12">
+    <div class="my-12">
       <CustomUsersTable :users="users" />
     </div>
   </div>
 </template>
 
 <script setup>
+import UsersService from "~/services/users";
+
+const usersAPI = new UsersService();
+
 const search = ref("");
 
-const users = ref([
-  {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-      street: "Kulas Light",
-      suite: "Apt. 556",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      geo: {
-        lat: "-37.3159",
-        lng: "81.1496",
-      },
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    company: {
-      name: "Romaguera-Crona",
-      catchPhrase: "Multi-layered client-server neural-net",
-      bs: "harness real-time e-markets",
-    },
-  },
-  // Add more user objects as needed...
-]);
+const users = ref([]);
 
 const handleSearch = () => {
   console.log("Searching for:", search.value);
 };
+
+const getData = () => {
+  usersAPI.getUsers().then((res) => {
+    if (res) {
+      users.value = res.data;
+    }
+  });
+};
+
+onMounted(() => {
+  getData();
+});
 </script>
 
 <style scoped>
